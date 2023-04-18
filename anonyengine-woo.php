f4c7b8c48d22b52ba8643b780fad4c04dc1c8c39
unknown <mo7amed.maki@gmail.com>
2023-04-18 14:53:59 +0200

    Update version

--- anonyengine-woo.php
+++ anonyengine-woo.php
@@ -4,7 +4,7 @@ if ( !defined('ABSPATH') ) exit();
  * Plugin Name: Optical products
  * Plugin URI: https://makiomar.com
  * Description: Add a new optical product type for woocommerce
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Mohammad Omar
  * Author URI: https://makiomar.com
  * Text Domain: wcpt
